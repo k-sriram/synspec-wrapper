85c87579728b93bcfd2c6ff4dc183f0c9ef32ae5
Sriram Krishna <ksri91@gmail.com>
2022-03-17 19:06:41 +0530

    🩹 utils.symlinkf won't link to itself

--- src/synspec/utils.py
+++ src/synspec/utils.py
@@ -6,6 +6,8 @@ def symlinkf(
 ) -> None:
     """Symlink a file. If the file already exists, delete it first."""
     dst = Path(dst)
+    if dst.resolve() == Path(src).resolve():
+        raise ValueError(f"src and dst are the same: {dst.resolve()}")
     if dst.exists():
         dst.unlink()
     dst.symlink_to(src, target_is_directory=target_is_directory)
