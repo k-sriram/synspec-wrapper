96f32bb955e5c13e66943a479695e99618e6e813
Sriram Krishna <ksri91@gmail.com>
2022-03-17 18:22:35 +0530

    ✅ Raise when files are missing

--- src/synspec/synspec.py
+++ src/synspec/synspec.py
@@ -11,10 +11,18 @@ class Synspec:
 
     def run(self, model: str) -> None:
         """Runs synspec with the given model"""
+        self.check_files(model)
         symlinkf(f"{model}.7", "fort.8")
 
         with open(f"{model}.5") as modelinput:
             subprocess.run([self.synspec], stdin=modelinput, check=True)
+
+    def check_files(self, model: str) -> None:
+        """Checks if the required files exist."""
+        files = ["fort.19", "fort.55", "{model}.5", "{model}.7"]
+        for file in files:
+            if not Path(file.format(model=model)).exists():
+                raise FileNotFoundError(f"{file.format(model=model)} not found")
 
 
 # Utils
